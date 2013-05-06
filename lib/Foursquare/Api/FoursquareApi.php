c8e41792f281f44ff1f44cd6391739fd34c3de0b
ner0tic <david.durost@gmail.com>
2013-05-05 22:00:41 -0400

    updates to reflection changes made upstream

--- lib/Foursquare/Api/FoursquareApi.php
+++ lib/Foursquare/Api/FoursquareApi.php
@@ -5,23 +5,65 @@ use Foursquare\Client;
 
 class FoursquareApi extends Client
 {
-    protected $client = null;
+    /**
+     *
+     * @var GeoPoint\Client $client 
+     */
+    protected $client;
     
+    /**
+     * 
+     * @param \GeoPoint\Client $client
+     */
     public function __construct( Client $client = null ) 
     {
         $this->client = $client instanceof Client ? $client : new Client();
 
-        $this->client->setUrl( 'https://api.foursquare.com/v2/:path' );
-        $this->client->getHttpClient()->setOption( 'certificate', false ); // 'Resources/config/certificate.pem' );
-        
-/** @TODO read in vars and set then from a yaml file **/        
-//        $this->client->setOption( 'login', '');
-//        $this->client->setOption( 'secret', '');
-        
+        $this->setUrl( 'https://api.foursquare.com/v2/:path' );
+                              
+    }    
+    
+    public function getClient()
+    {
+        return $this->client;
+    }
+    
+    public function setUrl( $url ) 
+    {
+        return $this->client->setUrl( $url );
+    }
+    
+    public function setOption( $k, $v )
+    {
+        return $this->client->setOption( $k, $v );
+    }
+    
+    public function get($path, $parameters = array(), $requestOpts = array()) {
+        return $this->client->get( $path, $parameters, $requestOpts);
+    }
+    
+    public function api( $api )
+    {
+        return $this->client->api( $api );
     }
     
     public function authenticate( $login, $secret = null, $method = null )
     {
-        return $this->client->authenticate( $login, $secret, $method );
+        $this->client->setOption( 'auth_method', $method );
+
+        if( $method === 'http_password' || 
+            $method === 'url_client_id' ) 
+        {
+            $this->client
+                 ->setOption( 'login', $login )
+                 ->setOption( 'secret', $secret );
+        } 
+        else 
+            $this->client->setOption( 'token', $secret );
     }
-}
+    
+    public function deauthenticate()
+    {
+        $this->authenticate( null, null, null );
+    }
+    
